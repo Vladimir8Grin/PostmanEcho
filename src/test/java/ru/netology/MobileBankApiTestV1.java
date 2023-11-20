a1702ac52603e528ba22102a51f7a7cb246bff0c
“Vladimir_Grin” <mortywallet@gmail.com>
2023-11-20 23:46:12 +0300

    PostmanEcho

--- src/test/java/ru/netology/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/MobileBankApiTestV1.java
@@ -3,19 +3,20 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Проект")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body(containsString("Проект"));
+
     }
 }
